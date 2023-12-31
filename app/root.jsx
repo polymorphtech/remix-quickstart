07c1f552f26fa195604501a09e554cedbc623cbe
Polymorph <team@polymorphtech.xyz>
2023-12-31 15:49:28 -0500

    First commit

---
+++ app/root.jsx
@@ -0,0 +1,30 @@
+import {
+    Links,
+    LiveReload,
+    Meta,
+    Outlet,
+    Scripts,
+  } from "@remix-run/react";
+  
+  export default function App() {
+    return (
+      <html>
+        <head>
+          <link
+            rel="icon"
+            href="data:image/x-icon;base64,AA"
+          />
+          <Meta />
+          <Links />
+        </head>
+        <body>
+          <h1>Hello world dynamic upon save4!</h1>
+          <Outlet />
+  
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+  
